a38b70c357a1ddffed9d3376e7807122fdd82787
DrawThis <drawthis6@gmail.com>
2024-11-21 16:18:43 -0500

    version 7

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,31 +1,30 @@
-import { db } from '$lib/server/database/client'
-import { usuarios } from '$lib/server/database/schema'
+import { db } from '$lib/server/database/connection'
+import { usuarios } from '$lib/server/database/data'
 import { eq } from 'drizzle-orm'
-import { redirect } from '@sveltejs/kit'
+
 
 export const handle = async ({ event, resolve }) => {
-    const session = event.cookies.get('session');
 
-    // Verificar si hay sesión activa
-    if (session) {
-        const user = await db.select().from(usuarios).where(eq(usuarios.token, session));
+    const session = event.cookies.get('session')
 
-        if (user && user.length > 0) {
-            event.locals.user = user[0];
-
-            // Si el usuario ya está autenticado y está intentando acceder a la página de login, redirigirlo a la landing page
-            if (event.url.pathname === '/') {
-                throw redirect(302, '/');
-            }
-        } else {
-            // Eliminar la cookie si el token no es válido
-            event.cookies.set('session', '', {
-                path: '/',
-                expires: new Date(0),
-            });
-        }
+    if (!session) {
+        // Si no hay sesión, cargue la página normalmente
+        return await resolve(event)
     }
 
-    // Continuar con la resolución de la solicitud
-    return await resolve(event);
-};
+    const user = await db.select().from(usuarios).where(eq(usuarios.token, session));
+
+    if (!user || user.length === 0) {
+        // Si la sesión no es válida, elimine la cookie y cargue la página normalmente
+        event.cookies.set('session', '', {
+            path: '/',
+            expires: new Date(0),
+          }
+        )
+        return await resolve(event)    
+    }
+
+    event.locals.user = user[0]
+
+    return await resolve(event)
+}
